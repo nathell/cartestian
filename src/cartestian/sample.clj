7f94bdbb8ea0b5c3cffc1659d1a3e8b302fd40ac
Daniel Janus <dj@danieljanus.pl>
2022-08-20 16:42:58 +0200

    Fix Vitter’s Method A
    I contrasted my implementation with the one that’s part of the
    TypeScript code at https://github.com/gliese1337/vitter-sample,
    and found a few bugs:
    
      - In the inner loop (my how-many-to-skip), top and Nreal need
        to decrease on every iteration of the loop.
      - After skipping s numbers, Nreal needs to be adjusted by s+1,
        but top only by s.
      - The code generated a smaller sample than asked for (an
        off-by-two error).
    
    I also renamed the local symbols to match those from Vitter’s
    pseudo-code.
    
    The implementation seems to be reasonably fast: (method-a 100 1000000)
    takes ~10 ms on my Mac Mini. I guess I can defer implementing
    Method D until later.

--- src/cartestian/sample.clj
+++ src/cartestian/sample.clj
@@ -1,20 +1,20 @@
 (ns cartestian.sample)
 
-(defn- how-many-to-skip [top n]
-  (let [v (rand)]
-    (loop [s 0 q (/ (double top) n)]
-      (if (<= q v)
+(defn- how-many-to-skip [top Nreal]
+  (let [V (rand)]
+    (loop [s 0 top top Nreal Nreal q (/ (double top) Nreal)]
+      (if (<= q V)
         s
-        (recur (inc s) (/ (* q top) n))))))
+        (recur (inc s) (dec top) (dec Nreal) (/ (* q (dec top)) (dec Nreal)))))))
 
-(defn method-a [n max]
+(defn method-a [k N]
   (->>
    (reduce
-    (fn [[v curr nreal top] n]
-      (let [s (how-many-to-skip top nreal)
-            nxt (+ curr s 1)]
-        [(conj! v nxt) nxt (- nreal s) (- top s)]))
-    [(transient []) 0 max (- max n)]
-    (range n 2 -1))
+    (fn [[v curr Nreal top] k]
+      (let [s (how-many-to-skip top Nreal)
+            nxt (+ curr s)]
+        [(conj! v nxt) (inc nxt) (- Nreal s 1) (- top s)]))
+    [(transient []) 0 N (- N k)]
+    (range k 0 -1))
    first
    persistent!))
